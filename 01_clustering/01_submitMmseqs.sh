fcbfbd8de51930cb3adeb24ce7b6ba6601fe35c6
stephkoest <stephan.koe@gmx.at>
2025-01-30 12:10:32 +0100

    removed typo mmseqs search

--- 01_clustering/01_submitMmseqs.sh
+++ 01_clustering/01_submitMmseqs.sh
@@ -4,4 +4,4 @@ mmseqs createdb data/Asgard_DB.faa data/Asgard_DB
 mmseqs createdb data/asCOG_seq_domains.faa data/asCOG_seq_domains_db
 grep ">" data/Asgard_DB.faa | sed 's/>//' | cut -d" " -f1 > data/Asgard_DB.txt
 mkdir -p log
-sbatch bin/runMmseqsProfile.sh data/Asgard_DB asCOG_seq_domains_db
+sbatch bin/runMmseqsProfile.sh data/Asgard_DB data/asCOG_seq_domains_db
